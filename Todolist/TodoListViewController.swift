242cc13e4bf9f5dc07d6a3bf90e18f8e57cd32df
Fadillah Hadiyanto <mofadillah@gmail.com>
2018-10-27 22:53:55 +0700

    Add new item functionality conplete

--- Todolist/TodoListViewController.swift
+++ Todolist/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let itemArray = ["Cari Kang Cecep", "Beli Kopi", "Jadikan IT Holding"]
+    var itemArray = ["First Item"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,6 +39,26 @@ class TodoListViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    //MARK - Add New Item
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Todolist Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //Happen when add button clicked
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Item Name"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert,animated: true,completion: nil)
+    }
+    
 
 }
 
